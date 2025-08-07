97415d1a7cfea094648d959043ddb391a7e4dabf
Andresousaaa1 <andredasneves761@gmail.com>
2025-08-07 13:37:39 -0300

    Arrumando repositorio com os codigos, Umain e Uestudantes

---
+++ Uestudantes.pas
@@ -0,0 +1,18 @@
+unit Uestudantes;
+
+interface
+
+  type TEstudante = class (Tobject)
+
+  public
+  Codigo: integer;
+  Nome: String;
+  end;
+
+
+
+implementation
+
+uses System.Generics.Collections;
+
+end.
